2937a9acb5f22d68483876af0f0526a0b1501936
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-17 18:42:23 +0900

    コード修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only:[:index, :show]
+  before_action :authenticate_user!, only:[:index, :show, :new, :edit]
 
   def index
     @prototypes = Prototype.includes(:user)
